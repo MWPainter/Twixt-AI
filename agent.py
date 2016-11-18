ef52f6452a009aa82db395a9b26bf8125b8a24d9
bardia22 <bardia.beigi@gmail.com>
2016-11-17 17:41:24 -0800

    fixed agent.py

--- agent.py
+++ agent.py
@@ -1,35 +1,44 @@
 import twixt
 
 class MinimaxAgent(object):
-    def __init__(self, depth = '2'):
-        self.index = 0
-	self.depth = int(depth)
+	def __init__(self, depth = '2'):
+		self.index = 0
+		self.depth = int(depth)
 
-    def evaluationFunction(self, currentGameState):
-	return currentGameState.getScore()
+	def evaluationFunction(self, currentGameState):
+		return currentGameState.getScore()
 
-    def getAction(self, gameState):
-	def oppMove(gameState, currentDepth):
-	    legalMoves = gameState.getLegalAction(gameState.agent)
-	    if legalMoves == set([]):
-		return (self.evaluationFunction(gameState), None)
-	    optimalScoreActionPair = (float('inf'), None)
-	    for action in legalMoves:
-		score, optimalAction = agentMove(gameState.generateSuccessor(gameState.agent, action), currentDepth-1)
-		if score < optimalScoreActionPair[0]:
-		    optimalScoreActionPair = (score, action)
-            return optimalScoreActionPair
+	def getAction(self, gameState):
 
-        def agentMove(gameState, currentDepth):
-	    if (currentDepth == 0):
-		return (self.evaluationFunction(gameState), None)
-            legalMoves = gameState.getLegalAction(gameState.agent)
-	    if legalMoves == set([]):
-		return (self.evaluationFunction(gameState), None)
-	    optimalScoreActionPair = (-float('inf'), None)
-	    for action in legalMoves:
-		score, optimalAction = oppMove(gameState.generateSuccessor(gameState.agent, action), currentDepth)
-		if score > optimalScoreActionPair[0]:
-		    optimalScoreActionPair = (score, action)
-	    return optimalScoreActionPair
+		def oppMove(gameState, currentDepth):
+	
+			legalMoves = gameState.getLegalAction(gameState.agent)
+			if legalMoves == set([]):
+				return (self.evaluationFunction(gameState), None)
+	    
+			optimalScoreActionPair = (float('inf'), None)
+			for action in legalMoves:
+				score, optimalAction = agentMove(gameState.generateSuccessor(gameState.agent, action), currentDepth-1)
+				if score < optimalScoreActionPair[0]:
+					optimalScoreActionPair = (score, action)
+
+			return optimalScoreActionPair
+
+		def agentMove(gameState, currentDepth):
+			
+			if (currentDepth == 0):
+				return (self.evaluationFunction(gameState), None)
+            
+			legalMoves = gameState.getLegalAction(gameState.agent)
+			if legalMoves == set([]):
+				return (self.evaluationFunction(gameState), None)
+	    
+			optimalScoreActionPair = (-float('inf'), None)
+			for action in legalMoves:
+				score, optimalAction = oppMove(gameState.generateSuccessor(gameState.agent, action), currentDepth)
+				if score > optimalScoreActionPair[0]:
+				optimalScoreActionPair = (score, action)
+	    
+			return optimalScoreActionPair
+
         return agentMove(gameState, self.depth)[1]
