d53e3e42fda89704c86b15b4274ba7c6c7933385
bardia22 <bardia.beigi@gmail.com>
2016-11-17 20:22:30 -0800

    added graphics

--- run.py
+++ run.py
@@ -1,16 +1,40 @@
-import twixt
+from twixt import twixtBoard
 import agent
+from util import printc, bcolors
 
-tb = twixt.twixtBoard(5)
-iteration = 1
-agent = [agent.MinimaxAgent(2), agent.MinimaxAgent(1)]
+n = 10
+iter = 1
+depth = 2
+agent = [agent.MinimaxAgent(depth), agent.MinimaxAgent(depth)]
+
+def drawBoard(n, labels, assignments):
+	for i in range(n):
+		for j in range(n):
+			if (i == 0 or i == n-1) and (j == 0 or j == n-1):
+				print "%-*s" % (8, ""),
+			else:
+				if (i,j) in labels:
+					if labels[(i,j)] in assignments[0]:
+						printc("%-*s" % (8, assignments[0][labels[(i,j)]]))
+					elif labels[(i,j)] in assignments[1]:
+						printc("%-*s" % (8, assignments[1][labels[(i,j)]]), bcolors.FAIL),
+				else:
+					print "%-*s" % (8, "x"),
+			if j == n-1:
+				print "\n"
+
+tb = twixtBoard(n)
 while tb.winner() == -1:
-    print "iteration: ", iteration
+    print "iteration: ", iter
+    iter += 1
+
+
     action = agent[tb.agent].getAction(tb)
-    print "agent, action: ", tb.agent, action
+    #print "agent, action: ", tb.agent, action
     tb.updateBoard(action)
-    print "pins: ", tb.pins
-    print "bridges: ", tb.bridges
-    print tb.winner()
-    iteration += 1
+    #print "pins: ", tb.pins
+    #print "bridges: ", tb.bridges
+    drawBoard(n, tb.label, tb.assignment)
+
+print tb.winner()
 
